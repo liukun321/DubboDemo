eaae95168e670bcf618e7b0a17c241fa21e11ec6
liukun321 <cmlk123@126.com>
2018-07-06 10:36:39 +0800

    添加redis缓存模块

--- mixiusi-service/src/main/java/com/mixiusi/service/CoffeeInfoServiceImpl.java
+++ mixiusi-service/src/main/java/com/mixiusi/service/CoffeeInfoServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.mixiusi.bean.CoffeeInfo;
 import com.mixiusi.bean.vo.CoffeeVo;
@@ -14,6 +15,8 @@ import com.mixiusi.biz.CoffeeInfoBiz;
 public class CoffeeInfoServiceImpl implements CoffeeInfoService {
 	@Autowired
 	private CoffeeInfoBiz coffeeInfoBiz;
+	@Reference(version = "1.0.0")
+	private RedisService redisService;
 	@Override
 	public CoffeeInfo addCoffeeInfo(CoffeeInfo coffeeInfo) {
 		return coffeeInfoBiz.addCoffeeInfo(coffeeInfo);
@@ -44,9 +47,16 @@ public class CoffeeInfoServiceImpl implements CoffeeInfoService {
 		return coffeeInfoBiz.queryCoffeeInfoSum(cvo);
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<CoffeeInfo> queryCoffeeInfo(CoffeeVo cvo) {
-		return coffeeInfoBiz.queryCoffeeInfo(cvo);
+		//1 查询缓存中是否存在，存在则从缓存中获取，否则从数据库中获取并在缓存中保存数据
+		List<CoffeeInfo> result = (List<CoffeeInfo>) redisService.get(CoffeeInfoService.class.getSimpleName() + "-queryCoffeeInfo");
+		if(null == result) {
+			result = coffeeInfoBiz.queryCoffeeInfo(cvo);
+			redisService.set(CoffeeInfoService.class.getSimpleName() + "queryCoffeeInfo", result);
+		}
+		return result;
 	}
 
 }
